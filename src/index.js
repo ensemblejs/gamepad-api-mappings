7de203ed77b28edb9765d63e257d07d325ba6e4d
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-01-25 21:34:02 +1100

    use a string to determine which deadzone calc you want

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 'use strict';
 
-export let getMapping = require('./get-mapping').getMapping;
-export let getAxial = require('./deadzones').getAxial;
-export let getScaledAxial = require('./deadzones').getScaledAxial;
-export let getRadial = require('./deadzones').getRadial;
-export let getScaledRadial = require('./deadzones').getScaledRadial;
+export const getMapping = require('./get-mapping').getMapping;
+export const getAxial = require('./deadzones').getAxial;
+export const getScaledAxial = require('./deadzones').getScaledAxial;
+export const getRadial = require('./deadzones').getRadial;
+export const getScaledRadial = require('./deadzones').getScaledRadial;
 export const deadZones = require('./mapping.json').deadZones;
+
+const deadzoneOptions = {
+  'axial': function axial (stick, deadzone) {
+    return {
+      x: getAxial(stick.x, deadzone),
+      y: getAxial(stick.y, deadzone)
+    };
+  },
+  'scaled-axial': function scaledAxial (stick, deadzone) {
+    return {
+      x: getScaledAxial(stick.x, deadzone),
+      y: getScaledAxial(stick.y, deadzone)
+    };
+  },
+  'radial': getRadial,
+  'scaled-radial': getScaledRadial
+};
+
+export function pickDeadzone (option) {
+  return deadzoneOptions[option];
+}
