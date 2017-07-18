1cb4362ecd168b6b913e65f364bbe39f922d0b51
gaspard <gaspard@gmail.com>
2017-07-18 17:09:28 +0200

    fix #1 parse all mapping, then default to standard

--- src/get-mapping.js
+++ src/get-mapping.js
@@ -3,9 +3,6 @@
 const mappingTable = require('./mapping.json');
 
 export function getMapping (id, mapping) {
-  if (mapping === 'standard') {
-    return mappingTable.standard;
-  }
 
   var deviceMap = mappingTable[`${id}-${mapping}`];
   if (!deviceMap) {
